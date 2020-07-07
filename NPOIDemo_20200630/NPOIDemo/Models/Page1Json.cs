eea916ef351778115c266a377fe16b42309fd27d
DESKTOP-OQGOHV0\YOUNG <y20051201@gmail.com>
2020-07-07 13:37:46 +0800

    test commit for NPOIDemo

---
+++ NPOIDemo_20200630/NPOIDemo/Models/Page1Json.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace NPOIDemo.Models
+{
+    public class Page1Json
+    {
+    }
+
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// To the full taiwan date.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <returns></returns>
+        public static string ToFullTaiwanDate(this DateTime datetime)
+        {
+            TaiwanCalendar taiwanCalendar = new TaiwanCalendar();
+
+            return string.Format(" {0} 年 {1} 月 {2} 日",
+                taiwanCalendar.GetYear(datetime),
+                datetime.Month,
+                datetime.Day);
+        }
+
+        /// <summary>
+        /// To the simple taiwan date.
+        /// </summary>
+        /// <param name="datetime">The datetime.</param>
+        /// <returns></returns>
+        public static string ToSimpleTaiwanDate(this DateTime datetime)
+        {
+            TaiwanCalendar taiwanCalendar = new TaiwanCalendar();
+
+            return string.Format("{0}/{1}/{2}",
+                taiwanCalendar.GetYear(datetime),
+                datetime.Month,
+                datetime.Day);
+        }
+    }
+
+
+    public class Rootobject
+    {
+        public Datum[] data { get; set; }
+    }
+
+    public class Datum
+    {
+        public string date { get; set; }
+        public int status { get; set; }
+        public int next_audit { get; set; }
+        public int weather { get; set; }
+        public Officer officer { get; set; }
+        public Transaction[] transaction { get; set; }
+        public Inventory[] inventory { get; set; }
+        public Others[] others { get; set; }
+        public string note { get; set; }
+        public string approve { get; set; }
+        public Pass_In_Out[] pass_in_out { get; set; }
+    }
+
+    public class Officer
+    {
+        public Main main { get; set; }
+        public Sub sub { get; set; }
+    }
+
+    public class Main
+    {
+        public string give { get; set; }
+        public string recieve { get; set; }
+    }
+
+    public class Sub
+    {
+        public string give { get; set; }
+        public string recieve { get; set; }
+    }
+
+    public class Transaction
+    {
+        public string item { get; set; }
+        public string amount { get; set; }
+        public bool give { get; set; }
+        public bool recieve { get; set; }
+    }
+
+    public class Inventory
+    {
+        public string title { get; set; }
+        public Item[] items { get; set; }
+        public Liaison liaison { get; set; }
+    }
+
+    public class Liaison
+    {
+        public string morning { get; set; }
+        public string afternoon { get; set; }
+        public string evening { get; set; }
+        public string midnight { get; set; }
+    }
+
+    public class Item
+    {
+        public string item { get; set; }
+        public bool check { get; set; }
+    }
+
+    public class Others
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string remark { get; set; }
+    }
+
+    public class Pass_In_Out
+    {
+        public string unit { get; set; }
+        public string place { get; set; }
+        public Firm_Leader firm_leader { get; set; }
+        public int amount { get; set; }
+        public string work { get; set; }
+        public Oversee oversee { get; set; }
+        public string remark { get; set; }
+    }
+
+    public class Firm_Leader
+    {
+        public string name { get; set; }
+        public string tel { get; set; }
+    }
+
+    public class Oversee
+    {
+        public string name { get; set; }
+        public string tel { get; set; }
+    }
+
+}
---
+++ NPOIDemo_20200630/NPOIDemo/Models/RequestObj.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NPOIDemo.Models
+{
+    public class RequestObj
+    {
+        public ReInventory[] inventory { get; set; }
+        public ReOTHER[] OTHER { get; set; }
+        public RePassinout[] Passinout { get; set; }
+        public ReTran[] Trans { get; set; }
+        public int id { get; set; }
+        public string date { get; set; }
+        public int status { get; set; }
+        public int next_audit { get; set; }
+        public int weather { get; set; }
+        public string officer_main_give { get; set; }
+        public string officer_sub_give { get; set; }
+        public string note { get; set; }
+    }
+
+
+    public class ReInventory
+    {
+        public Inventoryitem[] inventoryitems { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string liaisonmorning { get; set; }
+        public string liaisonevening { get; set; }
+        public int dutylog_id { get; set; }
+    }
+
+    public class Inventoryitem
+    {
+        public int id { get; set; }
+        public string item { get; set; }
+        public int checking { get; set; }
+        public int inventory_id { get; set; }
+    }
+
+    public class ReOTHER
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string remark { get; set; }
+        public int dutylog_id { get; set; }
+    }
+
+    public class RePassinout
+    {
+        public int id { get; set; }
+        public string unit { get; set; }
+        public string place { get; set; }
+        public string firm_leader_name { get; set; }
+        public string firm_leader_tel { get; set; }
+        public int amount { get; set; }
+        public string works { get; set; }
+        public string oversee_name { get; set; }
+        public string oversee_tel { get; set; }
+        public string remark { get; set; }
+        public int dutylog_id { get; set; }
+    }
+
+    public class ReTran
+    {
+        public int id { get; set; }
+        public string item { get; set; }
+        public string amount { get; set; }
+        public byte give { get; set; }
+        public byte recieve { get; set; }
+        public int dutylog_id { get; set; }
+    }
+
+
+}
